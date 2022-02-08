bc7eb8fb07a1d094eae49e98ed32a18cb4744bab
LeeGeonJae <wnwkrdkemf@gmail.com>
2022-02-08 13:56:44 +0900

    SDL 색깔

--- main.cpp
+++ main.cpp
@@ -65,9 +65,38 @@ int SDL_main(int argc, char* argv[])
 		//빨간색 선 그리기
 		SDL_SetRenderDrawColor(MyRenderer, 0xff, 0, 0, 128);
 		SDL_RenderDrawLine(MyRenderer, 100, 100, 200, 200);
+		
+		//녹색 사각형 그리기
+		SDL_SetRenderDrawColor(MyRenderer, 0x00, 0xff, 0, 0);
+		SDL_Rect MyRect = { 100, 100, 200, 200 };
+		SDL_RenderDrawRect(MyRenderer, &MyRect);
 
+		//파란색 채워진 사각형 그리기
+		SDL_SetRenderDrawColor(MyRenderer, 0x00, 0x0, 0xff, 0);
+		SDL_Rect MyRect2 = { 300, 200, 200, 200 };
+		SDL_RenderFillRect(MyRenderer, &MyRect);
 
+		//노란색 원 그리기
 
+		int Radius = 50;
+		int PositionX = 350;
+		int PositionY = 350;
+		SDL_SetRenderDrawColor(MyRenderer, 0xff, 0xff, 0x00, 0);
+		int OldX = sin(3.14f / 180.0f * 0.0f) * Radius;
+		int OldY = cos(3.14f / 180.0f * 0.0f) * Radius;
+
+		for (int Angle = 1; Angle < 360; ++Angle)
+		{
+			int X1 = OldX;
+			int Y1 = OldY;
+			int X2 = sin(3.14f / 180.0f * Angle)* Radius;
+			int Y2 = cos(3.14f / 180.0f * Angle)* Radius;
+
+			SDL_RenderDrawLine(MyRenderer, X1+PositionX, Y1+ PositionY, X2+PositionX, Y2+PositionY);
+			OldX = X2;
+			OldY = Y2;
+
+		}
 
 
 
